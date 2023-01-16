f8007f2f12628cd21f29f01799caacd710ae1b45
lisazborowski <elis.zbar@gmail.com>
2023-01-16 12:40:49 +0200

    rebased

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { db } from '../Firebase-config';
-import { collection, getDocs, doc, addDoc, getDoc, deleteDoc } from 'firebase/firestore';
+import { collection, getDocs, doc, addDoc, getDoc, deleteDoc, setDoc } from 'firebase/firestore';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { NavLink } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
-import Card from 'react-bootstrap/Card'; import { FormGroup } from "react-bootstrap";
-;
+import { Storage } from "../App";
 
 function Task() {
 
@@ -19,46 +18,28 @@ function Task() {
     const [toDoPage, setToDoPage] = useState([])
     const [shoppingListPage, setShoppingListPage] = useState([])
     const [arrayListPage, setArrayListPage] = useState([])
-
-    const toDoCollectionRef = collection(db, 'to-do')
+    const [boardItems, setBoardItems] = useState()
     const shoppingListCollectionRef = collection(db, "shopping list")
     const arraysListCollectionRef = collection(db, 'lists')
 
-    const addToDotask = async () => {
-        await addDoc(toDoCollectionRef, { name: newToDoTask })
-        // window.location.reload(true);
-    }
+    const { data, setData } = useContext(Storage)
+
 
     const addItemShopList = async () => {
         await addDoc(shoppingListCollectionRef, { name: newItem, quantity: newQuantity })
-        // window.location.reload(true);
     }
 
-    useEffect(() => {
-        try {
-            const getToDoPage = async () => {
-                const data = await getDocs(toDoCollectionRef)
-                // console.log(data);
-                setToDoPage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-            }
-            getToDoPage()
-        }
-        catch (error) {
-            // console.log(error);
-        }
-    }, [])
 
     useEffect(() => {
         try {
-            const getShoppingListPage = async () => {
-                const data = await getDocs(shoppingListCollectionRef)
-                // console.log(data);
-                setShoppingListPage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+            const getToDoList = async () => {
+                const data = await getDocs(arraysListCollectionRef)
+                console.log(data);
+                setToDoPage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
             }
-            getShoppingListPage()
+            getToDoList()
         }
         catch (error) {
-            console.log(error);
         }
     }, [])
 
@@ -66,18 +47,15 @@ function Task() {
         try {
             const getArrayListPage = async () => {
                 const data = await getDocs(arraysListCollectionRef)
-                // console.log(data);
                 setArrayListPage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
             }
             getArrayListPage()
         }
         catch (error) {
-            // console.log(error);
         }
     }, [])
 
     const deleteToDoTask = async (id) => {
-        // console.log(id);
         const toDoDoc = doc(db, "to-do", id)
         await deleteDoc(toDoDoc)
         window.location.reload(true);
@@ -85,7 +63,6 @@ function Task() {
 
 
     const deleteShoppingItem = async (id) => {
-        // console.log(id);
         const shoppingItemDoc = doc(db, "shopping-list", id)
         await deleteDoc(shoppingItemDoc)
         window.location.reload(true);
@@ -93,108 +70,38 @@ function Task() {
 
     return (
         <div>
-            <Card border="secondary" style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-
-                            <Form.Label>
-                                Your to do list
-
-                            </Form.Label>
-
-                            <Form.Control type="text" placeholder="new to do task..."
-                                onChange={(event) => { setNewToDoTask(event.target.value) }} />
-
-                        </Form.Group>
-
-                        <Button variant="primary" type="button"
-                            onClick={() => addToDotask()}>
-                            Create new task
-                        </Button>
-
-                        <Form.Group>
-
-                            <Form.Control type="email" placeholder="new to do task..."
-                                onChange={(event) => { setNewToDoTask(event.target.value) }} />
-
-                        </Form.Group>
-
-                        <Button variant="primary" type="button"
-                            onClick={() => addToDotask()}>
-                            create new task
-                        </Button>
-                    </Form>
-
-
-                </Card.Body>
-            </Card>
-
             <h2>Your tasks:</h2>
-            {
-                toDoPage.map((task, index) => {
-                    return <div key={index}>
-                        <ul>
+            <input placeholder="your new task..."
+                onChange={(event) => { setData(event.target.value) }}>
+            </input>
+            <button type="button" >submit</button>
 
-                            <li><h3>{task.name}</h3></li>
-                            <button type="button" onClick={() => deleteToDoTask(task?.id)}>
-                                Delete task
-                            </button>
-                        </ul>
-                    </div>
-                })
+            {arrayListPage.map((task, index) => {
+                return <div key={index}>
+                    <ul>
+
+                        {task.tasks && task.tasks.map((task2, index) => {
+                            console.log(task2)
+                            return <li key={index}>{task2}</li>
+                        })}
+                    </ul>
+                </div>
             }
-            <h3>lisos
-
-            </h3>
-            {
-                arrayListPage.map((task, index) => {
-
-                    return <div key={index}>
-                        {console.log(task.tasks)}
-
-                        <ul>
-                            {task.tasks && task.tasks.map((task2, index) => {
-                                console.log(task2)
-                                return <p>{task2}</p>
-                            })}
-                            <li>
-                                <h3>{task.item}</h3>
-                            </li>
-
-                            <li>
-                                <h3>{task.quantity}</h3>
-                            </li>
-                        </ul>
-                    </div>
-                })
+            )
             }
-
-
-
-
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-
-                    <Form.Label> Your shopping list</Form.Label>
-                    <Form.Control type="text" placeholder="your item..."
-                        onChange={(event) => { setNewItem(event.target.value) }} />
-
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-
-                    <Form.Control type="number" placeholder="how much..."
-                        onChange={(event) => { setNewQuantity(event.target.value) }} />
-
-                </Form.Group>
-
-                <Button variant="primary" type="button"
-                    onClick={() => (addItemShopList)}>
-                    create new item
-                </Button>
-            </Form>
-
+            <h4>izumka</h4>
+            {arrayListPage.map((task, index) => {
+                return <div key={index}>
+                    <ul>
+                        {task.item && task.item.map((task4, index) => {
+                            console.log(task4)
+                            return <li key={index}>{task4}</li>
+                        })}
+                    </ul>
+                </div>
+            }
+            )
+            }
 
             <h1>Your shopping list</h1>
             <input placeholder="your item..."
